120e8712aa0f163ceed93cfbad571459b9f54a41
hbrutsae <hbrutsae@M-BAT453-LAB16.local>
2019-01-26 15:17:18 -0800

    fdsfs

---
+++ if-else/src/ifDemo.java
@@ -0,0 +1,24 @@
+
+public class ifDemo {
+	
+	public static void main(String[] args) {
+		int testscore=76;
+		char grade;
+		
+		if(testscore>=90) {
+			grade='A';
+		}else if(testscore>=80){
+			grade='B';
+		}else if(testscore>=70){
+			grade='C';
+		}else if(testscore>=60){
+			grade='D';
+		}else {
+		
+			grade='X';
+		}
+		System.out.println("Grade =" + grade);
+		
+	}
+
+}
